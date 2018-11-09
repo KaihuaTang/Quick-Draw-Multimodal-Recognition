8e8f90427c6585bbb0070628d14c793f5cd77e08
MML-GS9\kaihua <tkhchipaomian@gmail.com>
2018-11-09 13:42:49 +0800

    allow joint learning

--- install_package.sh
+++ install_package.sh
@@ -1,5 +1,8 @@
 
+conda install pytorch=0.4 torchvision cuda90 -c pytorch
 conda install jupyter
 conda install tqdm
 conda install h5py
+conda install tensorflow
 pip install ndjson
+pip install tensorboardX
