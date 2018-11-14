60ef9a414b4e20833a1fd1f5851e0ede07e8078e
DESKTOP-PS56VT3\Admin <tkhchipaomian@gmail.com>
2018-11-15 00:59:02 +0800

    update visualized test

--- install_package.sh
+++ install_package.sh
@@ -1,5 +1,9 @@
 
+conda install pytorch=0.4 torchvision cuda90 -c pytorch
 conda install jupyter
 conda install tqdm
 conda install h5py
+conda install tensorflow
+conda install matplotlib
 pip install ndjson
+pip install tensorboardX
